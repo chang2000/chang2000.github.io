96cb38daa67459bf76339193d78658bfd3300f60
WANG Tianchang Alex <wangtcalex@gmail.com>
2022-09-22 20:41:40 -0700

    adjust template

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// a little hack to deal with the default weird behavior of eslint init
+// eslint-disable-next-line no-undef
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
