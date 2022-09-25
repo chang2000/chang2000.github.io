bbd647df0ddbae55b245bd8dff51cbe655833134
WANG Tianchang Alex <wangtcalex@gmail.com>
2022-09-25 11:15:24 -0700

    feat: collection page done

--- scripts/main.js
+++ scripts/main.js
@@ -165,13 +165,113 @@
   })
 
   /**
-   * Following JS Code is written by Tianchang Wang, NOT included in the template
+   * Porfolio isotope and filter
    */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container')
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      })
+
+      let portfolioFilters = select('#portfolio-flters li', true)
+
+      on('click', '#portfolio-flters li', function (e) {
+        e.preventDefault()
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active')
+        })
+        this.classList.add('filter-active')
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        })
+      }, true)
+    }
+
+  })
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  // eslint-disable-next-line no-unused-vars
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  })
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  })
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content')
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function () {
+        let progress = select('.progress .progress-bar', true)
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        })
+      }
+    })
+  }
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  })
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter()
+
+
   on('click', '.about .about-clickable', function (e) {
     e.preventDefault()
     let hiddenElement = document.getElementById('billboard-toggle')
     console.log(hiddenElement)
     hiddenElement.style.display = 'block'
   }, true)
-
 })()
